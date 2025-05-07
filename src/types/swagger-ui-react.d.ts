cc6b607ad603484a7370de03f202537183090bc1
pjdurden <prajjwalchittori1@gmail.com>
2025-05-08 02:52:56 +0530

    skipping lint

--- src/types/swagger-ui-react.d.ts
+++ src/types/swagger-ui-react.d.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'swagger-ui-react' {
     import React from 'react';
     const SwaggerUI: React.ComponentType<any>;
     export default SwaggerUI;
   }
+  /* eslint-enable @typescript-eslint/no-explicit-any */
   
